4b3a03e5e16b2c3b3dc535d2db7d702bc18fac4d
amani-bn <amanialabdallah@yahoo.com>
2021-03-17 12:51:34 +0200

    exam3

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 // DB setup
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
-
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// testttttttt
 // routes
 app.get('/',homeHandler);
 app.get('/getCountryResult',getCountryResultHandler);
